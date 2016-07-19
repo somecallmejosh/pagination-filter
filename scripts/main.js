0930cb464c805396cfbc6a599bed5a45cf9a5699
Joshua Briley <josh@thebrileys.com>
2016-07-18 22:07:59 -0400

    Add pagination feature.

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,11 @@
   var itemsToShowPerPage = 10,
       page = document.querySelector('.page'),
       pagesRequired,
-      paginationTemplate = '<div class="pagination"><ul></ul></div>',
       studentItem = document.querySelectorAll('.student-item'),
       studentItemCount = 0,
       studentList = document.querySelector('.student-list');
 
+  // Determine the number of students
   function getStudentCount() {
     // Count the total number of students 'on the page';
     for(var i = 0; i < studentItem.length; i++) {
@@ -19,21 +19,27 @@
     return studentItemCount;
   }
 
+  // Determine the number of pages required to display 10 students per page
   function getPageCount() {
-    // Determine how many pages are required to display 10 students per page.
     var numberOfPagesRequired = Math.ceil(getStudentCount() / itemsToShowPerPage);
     return numberOfPagesRequired;
   }
 
+  // Build the pagination links
   function buildPaginationLinks() {
-    if(studentItemCount > itemsToShowPerPage) {
-
-    }
+    // Create the parent pagination contianer <div class="pagination">
+      var template = '<div class="pagination"><ul><li><a href="#">1</a></ul></div>',
+      // Create the unordered list
+      // Create the appropriate number of list items
+        // Create anchor links
+        // Add the list item idex (+1) as the content of the anchor link
+    // Append the pagination links to the .page container
   }
 
-  function showRequestedPage() {
+  // Hide all but the first 10 students when the page loads.
 
-  }
+  // Show the students that correspond to the pagination click;
+
 
   buildPaginationLiks();
 
